2e34b2110e96fd3e510d958d1115b86aa9cc24be
JamesRamm <jamessramm@gmail.com>
2017-02-05 11:31:25 +0000

    Fulfill view based on InspectView

--- longclaw/orders/wagtail_hooks.py
+++ longclaw/orders/wagtail_hooks.py
@@ -1,8 +1,69 @@
+from django.contrib.admin.utils import quote
+from django.utils.translation import ugettext as _
+from django.conf.urls import url
+
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, modeladmin_register
 )
-
+from wagtail.contrib.modeladmin.helpers import ButtonHelper
+from wagtail.contrib.modeladmin.views import InspectView
 from longclaw.orders.models import Order
+
+class OrderButtonHelper(ButtonHelper):
+
+    fulfill_button_classnames = []
+    cancel_button_classnames = ['no']
+
+    def cancel_button(self, pk, classnames_add=None, classnames_exclude=None):
+        if classnames_add is None:
+            classnames_add = ['cancel-button']
+        if classnames_exclude is None:
+            classnames_exclude = []
+        classnames = self.cancel_button_classnames + classnames_add
+        cn = self.finalise_classname(classnames, classnames_exclude)
+        return {
+            'url': '',
+            'label': _('Cancel'),
+            'classname': cn,
+            'title': _('Cancel this %s') % self.verbose_name,
+        }
+
+    def fulfill_button(self, pk, classnames_add=None, classnames_exclude=None):
+        if classnames_add is None:
+            classnames_add = ['fulfill-button']
+        if classnames_exclude is None:
+            classnames_exclude = []
+        classnames = self.fulfill_button_classnames + classnames_add
+        cn = self.finalise_classname(classnames, classnames_exclude)
+        return {
+            'url': self.url_helper.get_action_url('fulfill', quote(pk)),
+            'label': _('Fulfill'),
+            'classname': cn,
+            'title': _('Fulfill this %s') % self.verbose_name,
+        }
+
+    def get_buttons_for_obj(self, obj, exclude=None, classnames_add=None,
+                            classnames_exclude=None):
+        if exclude is None:
+            exclude = []
+        if classnames_add is None:
+            classnames_add = []
+        if classnames_exclude is None:
+            classnames_exclude = []
+
+        ph = self.permission_helper
+        usr = self.request.user
+        pk = quote(getattr(obj, self.opts.pk.attname))
+        btns = []
+        if ph.user_can_inspect_obj(usr, obj):
+            btns.append(self.fulfill_button(pk, classnames_add, classnames_exclude))
+            btns.append(self.inspect_button(pk, classnames_add, classnames_exclude))
+            btns.append(self.cancel_button(pk, classnames_add, classnames_exclude))
+
+        return btns
+
+class FulfillView(InspectView):
+    pass
 
 class OrderModelAdmin(ModelAdmin):
     model = Order
@@ -13,6 +74,31 @@ class OrderModelAdmin(ModelAdmin):
     list_display = ('id', 'status', 'status_note', 'email', 'payment_date', 'total_items', 'total')
     list_filter = ('status', 'payment_date', 'email')
     inspect_view_enabled = True
+    fulfill_view_class = FulfillView
     index_template_name = 'orders_index.html'
+    button_helper_class = OrderButtonHelper
+
+    def fulfill_view(self, request, instance_pk):
+        """
+        Instantiates a class-based view to provide 'inspect' functionality for
+        the assigned model. The view class used can be overridden by changing
+        the 'inspect_view_class' attribute.
+        """
+        kwargs = {'model_admin': self, 'instance_pk': instance_pk}
+        view_class = self.fulfill_view_class
+        return view_class.as_view(**kwargs)(request)
+
+    def get_admin_urls_for_registration(self):
+        """
+        Utilised by Wagtail's 'register_admin_urls' hook to register urls for
+        our the views that class offers.
+        """
+        urls = super(OrderModelAdmin, self).get_admin_urls_for_registration()
+        urls = urls + (
+            url(self.url_helper.get_action_url_pattern('fulfill'),
+                self.fulfill_view,
+                name=self.url_helper.get_action_url_name('fulfill')),
+        )
+        return urls
 
 modeladmin_register(OrderModelAdmin)
