ae92abffcbe792d41ee7aafb08e59ba874f3a4c4
JamesRamm <jamessramm@gmail.com>
2017-02-07 21:01:33 +0000

    Fix migration dependencies

--- longclaw/basket/migrations/0003_auto_20170207_2053.py
+++ longclaw/basket/migrations/0003_auto_20170207_2053.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('basket', '0002_basketitem_product'),
+        ('basket', '0001_initial'),
     ]
 
     operations = [
