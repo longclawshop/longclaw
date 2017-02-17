78dfe80f2ae9a449482b74aefb17eefa6b53e735
JamesRamm <jamessramm@gmail.com>
2017-02-17 09:15:48 +0000

    Add productvariant model to template

---
+++ longclaw/project_template/products/models.py
@@ -0,0 +1,9 @@
+from longclaw.longclawproducts.models import ProductVariantBase
+
+class ProductVariant(ProductVariantBase):
+
+    # Enter your custom product variant fields here
+    # e.g. colour, size, stock and so on.
+    # Remember, ProductVariantBase provides 'price', 'ref', 'slug' fields
+    # and the parental key to the Product model.
+    pass
