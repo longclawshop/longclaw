324fdfb902f75466e42f1816859fc313191c232c
JamesRamm <jamessramm@gmail.com>
2017-05-31 16:04:44 +0100

    Test order model props

--- longclaw/longclaworders/tests.py
+++ longclaw/longclaworders/tests.py
@@ -3,10 +3,16 @@ from django.contrib.auth.models import User
 
 from longclaw.tests.utils import LongclawTestCase, OrderFactory
 
-class OrderApiTests(LongclawTestCase):
+class OrderTests(LongclawTestCase):
 
     def setUp(self):
         self.order = OrderFactory()
 
     def test_fulfill_order(self):
         self.post_test({}, 'longclaw_fulfill_order', urlkwargs={'pk': self.order.id})
+
+    def test_total(self):
+        self.assertEqual(self.order.total, 0)
+
+    def test_total_items(self):
+        self.assertEqual(self.order.total_items, 0)
