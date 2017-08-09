406ec27152e863ca0f232655560f30a5f0219081
JamesRamm <jamessramm@gmail.com>
2017-08-09 10:48:34 +0100

    Fix tests

--- longclaw/longclawcheckout/forms.py
+++ longclaw/longclawcheckout/forms.py
@@ -6,7 +6,7 @@ class CheckoutForm(forms.Form):
     Captures extra info required for checkout
     '''
     email = forms.EmailField()
-    shipping_option = forms.CharField(widget=forms.Select)
+    shipping_option = forms.CharField(widget=forms.Select, required=False)
     different_billing_address = forms.BooleanField(required=False)
     class Media:
         js = ('checkout.js',)
