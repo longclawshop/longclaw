23fb81402b325f88f58308c846027dfaba046cbe
JamesRamm <jamessramm@gmail.com>
2017-10-08 21:27:40 +0200

    Drop 1.8 support

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
+        'django>=1.9',
         'wagtail>=1.9',
         'django-countries>=4.3',
         'django-extensions>=1.7.5',
