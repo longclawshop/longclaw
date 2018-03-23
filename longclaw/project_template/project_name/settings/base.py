9e3edccf0000201d173f61d791be79403c626022
JamesRamm <jamessramm@gmail.com>
2018-03-23 22:11:09 +0100

    Switch to en-gb, en-us might not be recognized. #146

--- longclaw/project_template/project_name/settings/base.py
+++ longclaw/project_template/project_name/settings/base.py
@@ -118,7 +118,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/{{ docs_version }}/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
