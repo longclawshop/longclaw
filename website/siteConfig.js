68456b983a54cfd68d26f183c8ff53337e9f8fc5
JamesRamm <james@pop-os.localdomain>
2018-12-31 16:18:01 +0100

    Update config fo gh-pages

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,14 +24,14 @@ const siteConfig = {
   title: 'Longclaw', // Title for your website.
   tagline: 'E-commerce extension for Wagtail CMS',
   url: 'https://JamesRamm.github.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/longclaw/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
   projectName: 'longclaw',
-  organizationName: '',
+  organizationName: 'JamesRamm',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
