b7989b30e50f5d947968abe06cbddee82167b022
gordon pendleton <gordon@internal.gandssecurityincorporated.com>
2019-03-18 11:33:20 -0400

    add test for get_shipping_cost with a basket rate

--- longclaw/shipping/tests.py
+++ longclaw/shipping/tests.py
@@ -1,10 +1,16 @@
+import uuid
+
 from django.test import TestCase
 from django.forms.models import model_to_dict
-from longclaw.tests.utils import LongclawTestCase, AddressFactory, CountryFactory, ShippingRateFactory
+from longclaw.tests.utils import LongclawTestCase, AddressFactory, CountryFactory, ShippingRateFactory, BasketItemFactory
 from longclaw.shipping.forms import AddressForm
 from longclaw.shipping.utils import get_shipping_cost
 from longclaw.shipping.templatetags import longclawshipping_tags
 from longclaw.configuration.models import Configuration
+from longclaw.basket.utils import basket_id
+
+from .models import ShippingRate
+
 
 class ShippingTests(LongclawTestCase):
     def setUp(self):
@@ -37,6 +43,30 @@ class ShippingTests(LongclawTestCase):
         ls = Configuration(default_shipping_enabled=True)
         result = get_shipping_cost(ls)
         self.assertEqual(ls.default_shipping_rate, result["rate"])
+
+
+class ShippingBasketTests(LongclawTestCase):
+    def setUp(self):
+        """Create a basket with things in it
+        """
+        request = RequestFactory().get('/')
+        request.session = {}
+        self.bid = basket_id(request)
+        self.item = BasketItemFactory(basket_id=bid)
+        BasketItemFactory(basket_id=bid)
+        
+        self.rate1 = ShippingRate.objects.create(
+            name='98d17c43-7e20-42bd-b603-a4c83c829c5a',
+            rate=99,
+            carrier='8717ca67-4691-4dff-96ec-c43cccd15241',
+            description='313037e1-644a-4570-808a-f9ba82ecfb34',
+            basket_id=bid,
+        )
+    
+    def test_basket_rate(self):
+        result = get_shipping_cost(Configuration(), name='98d17c43-7e20-42bd-b603-a4c83c829c5a', basket_id=self.bid)
+        self.assertEqual(result["rate"], 99)
+        self.assertEqual(result["description"], '313037e1-644a-4570-808a-f9ba82ecfb34')
 
 
 class AddressFormTest(TestCase):
