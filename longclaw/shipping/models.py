02f018e6f6a7cd9006371b1f332778669ba05830
gordon pendleton <gordon@internal.gandssecurityincorporated.com>
2019-03-18 11:47:33 -0400

    add fields to allow us to tie shipping rate to a basket & address

--- longclaw/shipping/models.py
+++ longclaw/shipping/models.py
@@ -39,6 +39,8 @@ class ShippingRate(models.Model):
     carrier = models.CharField(max_length=64)
     description = models.CharField(max_length=128)
     countries = models.ManyToManyField('shipping.Country')
+    basket_id = models.CharField(blank=True, db_index=True, max_length=32)
+    address = models.ForeignKey(Address, blank=True, null=True, on_delete=models.PROTECT)
 
     panels = [
         FieldPanel('name'),
