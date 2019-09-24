6adccc87830aa75f4a35e2ab29e8235e49533ae2
gordon pendleton <gordon@internal.gandssecurityincorporated.com>
2019-09-24 12:21:28 -0400

    get tox to run

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -8,4 +8,9 @@ export DEBIAN_FRONTEND=noninteractive
 
 add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
-apt-get install -y tox python3.5 python3.6 python3.7
+apt-get install -y build-essential libssl-dev tox python3.5 python3.6 python3.7
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. $HOME/.nvm/nvm.sh
+nvm install stable
+nvm use stable
