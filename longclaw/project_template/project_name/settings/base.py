eb23863911d77b847d902ce6d709edf2fb9c2dc8
blake-g <blake@make.nz>
2020-11-11 16:51:44 +1300

    Update Django/Wagtail dependencies
    Minor changes to accompany updated django/wagtail versions

--- longclaw/project_template/project_name/settings/base.py
+++ longclaw/project_template/project_name/settings/base.py
@@ -64,7 +64,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 
-    'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
 
