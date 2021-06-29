454461832227ba329c4531fbeb0f7bd354ed4911
Jan Capek <jan@webtune.net>
2021-06-29 15:46:36 -0700

    fix: AppConfig full paths

--- longclaw/configuration/apps.py
+++ longclaw/configuration/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class LongclawSettingsConfig(AppConfig):
-    name = 'configuration'
+    name = 'longclaw.configuration'
     verbose_name = 'Longclaw Configuration'
