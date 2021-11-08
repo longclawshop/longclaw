7a95ed6fbdb565e95bca393453b9c1fabad5e13e
M.Jacobs <jacobs.michiel@icloud.com>
2021-11-08 20:04:51 +0100

    removed double assert

--- longclaw/core/templatetags/longclawcore_tags.py
+++ longclaw/core/templatetags/longclawcore_tags.py
@@ -9,16 +9,10 @@ VENDORS_PATH = os.path.join('core', 'js', 'vendors.bundle.js')
 
 @register.inclusion_tag("core/script.html")
 def longclaw_vendors_bundle():
-    assert os.path.exists(
-        os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', VENDORS_PATH)
-    )
     return {'path': VENDORS_PATH}
 
 @register.inclusion_tag("core/script.html")
 def longclaw_client_bundle():
-    assert os.path.exists(
-        os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', CLIENT_PATH)
-    )
     return {'path': CLIENT_PATH}
 
 @register.simple_tag
